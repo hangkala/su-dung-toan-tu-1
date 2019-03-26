af126f27c1af9a71cdc41c607a07fe7599b13147
hangkala <you@example.com>
2019-03-26 15:25:42 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var inputWidth
+var inputHeight
+
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+
+width = parseInt(inputWidth);
+height = parseInt(inputHeight);
+area = width*height;
+document.write("the area is: "+area);
